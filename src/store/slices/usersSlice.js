a4c0fdb97d389c29f9f4b4d12238cc72a46584a9
MonirSaikat <moniruzzamansaikat@yahoo.com>
2022-12-25 17:24:51 +0600

    added users slice

---
+++ src/store/slices/usersSlice.js
@@ -0,0 +1,12 @@
+const { createSlice } = require('@reduxjs/toolkit');
+
+const usersSlice = createSlice({
+  name: 'users',
+  initialState: {
+    data: []
+  },
+  reducers: {}
+});
+
+export const usersReducer = usersSlice.reducer;
+export const {} = usersSlice.actions;
