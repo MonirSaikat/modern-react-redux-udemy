4d2e89072876013b5af58fa13a90038c21ae146b
MonirSaikat <moniruzzamansaikat@yahoo.com>
2022-12-28 00:15:08 +0600

    remove user

--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -1,15 +1,25 @@
 import Button from './Button';
+import { useThunk } from '../hooks/use-thunk';
+import { GoTrashcan } from 'react-icons/go';
+import { removeUser } from '../store';
 
 function UsersListItem({ user }) {
+  const [doRemoveUser, isRemovingUser, removeUserError] = useThunk(removeUser);
   const handleRemove = () => {
-
+    doRemoveUser(user);
   };
 
   return (
     <div className='mb-2 border rounded'>
-      <span>{user.name}</span>
-      <div className='flex p-2 justify-between items-center'>
-        <Button onClick={handleRemove} danger>delete</Button>
+      <div className='flex p-2 items-center justify-start'>
+        <Button
+          className='mr-3'
+          loading={isRemovingUser}
+          onClick={handleRemove}
+        >
+          <GoTrashcan />
+        </Button>
+        <span>{user.name}</span>
       </div>
     </div>
   );
