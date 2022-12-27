3821be26d7046240f349281f94bc1ee7de0b6712
MonirSaikat <moniruzzamansaikat@yahoo.com>
2022-12-28 00:16:29 +0600

    pass user

--- src/store/thunks/removeUser.js
+++ src/store/thunks/removeUser.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const removeUser = createAsyncThunk('users/remove', async (id) => {
-  const response = await axios.delete('http://localhost:3005/users/' + id);
-  return id;
+export const removeUser = createAsyncThunk('users/remove', async (user) => {
+  await axios.delete(`http://localhost:3005/users/${user.id}`);
+  return user;
 });
